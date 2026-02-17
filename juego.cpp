8c9fb182a62c57126c6dd4e5a8c5b5d914ac4385
danielgarcia-j <00014325@uca.edu.sv>
2026-02-17 15:01:30 -0600

    feat: Menu inscribir jugador

--- juego.cpp
+++ juego.cpp
@@ -3,8 +3,15 @@
 using namespace std;
 
 
-
 int main() {
-   
+    int opcion;
+    do {
+        cout << "\n===== MENU PRINCIPAL =====\n";
+        cout << "1. Inscribir jugador\n";
+        cout << "0. Salir\n";
+        cout << "Seleccione opcion: ";
+        cin >> opcion;
+    } while (opcion != 0);
+
     return 0;
 }
