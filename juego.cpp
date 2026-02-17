0c1772c0aebf070439ed2511c8964dc5b2b8a355
danielgarcia-j <00014325@uca.edu.sv>
2026-02-17 15:16:20 -0600

    feat:implementar inscripción básica de jugador en lista

--- juego.cpp
+++ juego.cpp
@@ -30,27 +30,53 @@ public:
     void sumarPunto(int p) {
         puntaje += p;
     }
+};
+
 class ListaCircular {
-    private:
+private:
     Jugador* primero;
     int cantidad;
 
-    public:
+public:
     ListaCircular() {
         primero = nullptr;
         cantidad = 0;
     }
+
+    void inscribir(string nombre, string id) {
+        Jugador* nuevo = new Jugador(nombre, id);
+
+        if (primero == nullptr) {
+            primero = nuevo;
+        }
+
+        cantidad++;
+    }
 };
 
-};
 int main() {
+    ListaCircular lista;
     int opcion;
+    string nombre, id;
+
     do {
         cout << "\n===== MENU PRINCIPAL =====\n";
         cout << "1. Inscribir jugador\n";
         cout << "0. Salir\n";
         cout << "Seleccione opcion: ";
         cin >> opcion;
+        cin.ignore();
+
+        switch (opcion) {
+            case 1:
+                cout << "Ingrese nombre: ";
+                getline(cin, nombre);
+                cout << "Ingrese ID: ";
+                getline(cin, id);
+                lista.inscribir(nombre, id);
+                break;
+        }
+
     } while (opcion != 0);
 
     return 0;
