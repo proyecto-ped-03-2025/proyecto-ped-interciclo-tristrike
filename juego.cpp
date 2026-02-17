c7b703da65e48b9c8c235a7e81c36e138186cbdb
Jeffrey Guevara <00388525@uca.edu.sv>
2026-02-17 17:22:35 -0600

    add: ayuda sobre el juego

--- juego.cpp
+++ juego.cpp
@@ -1,23 +1,7 @@
-   void mostrarGanador() {
-        if (primero == nullptr) {
-            cout << "No hay jugadores.\n";
-            return;
-        }
-
-        Jugador* temp = primero;
-        Jugador* ganador = primero;
-
-        do {
-            if (temp->getPuntaje() > ganador->getPuntaje()) {
-                ganador = temp;
-            }
-            temp = temp->getSiguiente();
-        } while (temp != primero);
-
-        cout << "\n🏆 Ganador: " << ganador->getNombre()
-             << " (ID: " << ganador->getID() << ")"
-             << " con " << ganador->getPuntaje()
-             << " puntos.\n";
-    }
-};
-
+void ayuda() {
+    cout << "\n===== AYUDA =====\n";
+    cout << "1. Inscribe a los jugadores con su nombre y un ID.\n";
+    cout << "2. Verifica el listado de los jugadores inscritos.\n";
+    cout << "3. Comienza a realizar el juego que es un todos contra todos.\n";
+    cout << "4. Muestra el ganador del juego.\n";
+}
