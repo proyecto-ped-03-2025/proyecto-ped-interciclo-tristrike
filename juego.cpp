28a618ecb5c17e795edac7fb4dc7a89548ada713
Jeffrey Guevara <00388525@uca.edu.sv>
2026-02-17 16:53:55 -0600

    add: muestra ganador

--- juego.cpp
+++ juego.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
-#include <string>
-using namespace std;
+   void mostrarGanador() {
+        if (primero == nullptr) {
+            cout << "No hay jugadores.\n";
+            return;
+        }
 
+        Jugador* temp = primero;
+        Jugador* ganador = primero;
 
+        do {
+            if (temp->getPuntaje() > ganador->getPuntaje()) {
+                ganador = temp;
+            }
+            temp = temp->getSiguiente();
+        } while (temp != primero);
 
-int main() {
-   
-    return 0;
-}
+        cout << "\n🏆 Ganador: " << ganador->getNombre()
+             << " (ID: " << ganador->getID() << ")"
+             << " con " << ganador->getPuntaje()
+             << " puntos.\n";
+    }
+};
+
