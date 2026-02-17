fc8f91024ee8e9d55142e3b1e190478c94f73720
Jeffrey Guevara <00388525@uca.edu.sv>
2026-02-17 17:31:18 -0600

    add: integrantes del grupo

--- juego.cpp
+++ juego.cpp
@@ -1,7 +1,6 @@
-void ayuda() {
-    cout << "\n===== AYUDA =====\n";
-    cout << "1. Inscribe a los jugadores con su nombre y un ID.\n";
-    cout << "2. Verifica el listado de los jugadores inscritos.\n";
-    cout << "3. Comienza a realizar el juego que es un todos contra todos.\n";
-    cout << "4. Muestra el ganador del juego.\n";
+void integrantes() {
+    cout << "\n===== INTEGRANTES =====\n";
+    cout << "Eduardo Daniel Garcia Osorio - 00014325\n";
+    cout << "Jeffrey Alessandro Guevara Batres - 00388525\n";
 }
+
