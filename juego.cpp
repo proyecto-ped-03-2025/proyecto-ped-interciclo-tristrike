f1ba4839dff68130be506d3488b2f03162b2afba
danielgarcia-j <00014325@uca.edu.sv>
2026-02-17 15:07:52 -0600

    feat: crear clase jugador

--- juego.cpp
+++ juego.cpp
@@ -2,7 +2,35 @@
 #include <string>
 using namespace std;
 
+class Jugador {
+private:
+    string nombre;
+    string id;
+    int puntaje;
 
+public:
+    Jugador(string n, string i) {
+        nombre = n;
+        id = i;
+        puntaje = 0;
+    }
+
+    string getNombre() {
+        return nombre;
+    }
+
+    string getID() {
+        return id;
+    }
+
+    int getPuntaje() {
+        return puntaje;
+    }
+
+    void sumarPunto(int p) {
+        puntaje += p;
+    }
+};
 int main() {
     int opcion;
     do {
